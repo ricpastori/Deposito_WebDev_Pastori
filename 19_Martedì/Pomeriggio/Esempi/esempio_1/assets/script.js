ef64345fbb3320eb854fc2ffff4a6b775e0e1497
Riccardo Pastori <ricpastori@icloud.com>
2026-05-19 17:49:33 +0200

    Upload day 7 afternoon example fo J.H.C.

---
+++ 19_Martedì/Pomeriggio/Esempi/esempio_1/assets/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+	// Seleziona l'elemento tramite id
+	const elemento = document.getElementById("text");
+
+	// Modifica il contenuto
+	elemento.textContent = "Testo modificato!";
+}
