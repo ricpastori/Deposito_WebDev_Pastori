4a4315b8a6a15ef28374e1ecf6b8701a951ae64f
Riccardo Pastori <ricpastori@icloud.com>
2026-05-20 19:59:17 +0200

    Upload day 8 morning exercise with extras (except one, see TODO.md for more info)

---
+++ 20_Mercoledì/Mattina/Esercizi/esercizi_1/TODO.md
@@ -0,0 +1,28 @@
+# J.H.C - Esercizio
+Realizzare una pagina HTML con:
+- [x] un titolo
+- [x] un paragrafo
+- [x] un input di testo
+- [x] un bottone
+- [x] una lista vuota (ul)
+
+## Obiettivi - Con JavaScript bisogna:
+- [x] selezionare gli elementi del DOM tramite getElementById()
+- [x] leggere il valore scritto nell’input
+- [x] controllare che il campo non sia vuoto
+- [x] creare dinamicamente un nuovo elemento della lista (li)
+- [x] inserire il testo dell’input nel nuovo elemento
+- [x] aggiungere il nuovo elemento alla lista
+- [x] svuotare l’input dopo l’inserimento
+
+## Extra difficile - Aggiungere anche le seguenti funzionalità:
+- [x] un bottone per eliminare gli elementi della lista
+  > Ho fatto i bottoni interni alle card, non so se ho capito bene la consegna
+- [x] cambiare il colore degli elementi cliccati
+  > Lo considero fatto con il cambio lista
+- [x] mostrare quanti elementi sono presenti nella lista
+- [x] impedire l’inserimento di elementi duplicati
+- [x] aggiungere un tasto “Svuota lista”
+- [x] utilizzare classList.toggle() per attivare/disattivare uno stile
+- [ ] usare eventi multipli (click, mouseover, keydown)
+  > Per ora non lo faccio perché non saprei che funzionalità sensate creare per mouseover e keydown senza strafare
--- 20_Mercoledì/Mattina/Esercizi/esercizi_1/assets/script.js
+++ 20_Mercoledì/Mattina/Esercizi/esercizi_1/assets/script.js
@@ -1,16 +1,39 @@
-// Comunque qua, per fare gli extra, stavo già pensando al dinamismo e stavo andando a vedere il sessionStorage
-
 const addButton = document.getElementById("add-activity-button");
-const moveButtons = document.getElementsByClassName("move-activity-button");
-const removeButtons = document.getElementsByClassName("remove-activity-button");
-const todoList = document.getElementById("todo-list");
-const completedList = document.getElementById("completed-list");
+const clearButton = document.getElementById("clear-list-button");
+const cardCounter = document.getElementById("list-count");
+
+cardCounter.innerText = String(0);
 
 function tryAddActivity(activityName) {
+	const todoList = document.getElementById("todo-list");
+	const completedList = document.getElementById("completed-list");
+
+	// Questi array conterranno i valori degli attributi data-control già presenti.
+	// Servono per controllare se l'attività è già stata aggiunta.
+	const todoControlAttributes = [];
+	const completedControlAttributes = [];
+
+	for (const child of todoList.children) {
+		const controlAttribute = child.getAttribute("data-control");
+		todoControlAttributes.push(controlAttribute);
+	}
+
+	for (const child of completedList.children) {
+		const controlAttribute = child.getAttribute("data-control");
+		completedControlAttributes.push(controlAttribute);
+	}
+
 	if (activityName !== "") {
-		// Ho iniziato la struttura per fare gli extra. So che if (true) non ha senso
-		// if (activityList.contains(activityName) === false) {
-		if (true) {
+		/* Questa istruzione trasforma il nome in formato "kebab-case" per usarlo come valore di confronto */
+		kebabActivityName = activityName.replaceAll(" ", "-");
+
+		console.log(todoControlAttributes);
+
+		// Questo controllo verifica se l'attività è già presente prima di crearne una nuova.
+		if (
+			!todoControlAttributes.includes(kebabActivityName) &&
+			!completedControlAttributes.includes(kebabActivityName)
+		) {
 			createActivity(activityName);
 		} else {
 			alert("L'attività che vuoi aggiungere è già nella lista");
@@ -21,55 +44,89 @@ function tryAddActivity(activityName) {
 }
 
 function createActivity(activityName) {
+	// Le nuove attività vengono inserite sempre nella lista delle cose da fare.
+	const todoList = document.getElementById("todo-list");
+
 	const activityCard = document.createElement("li");
 	const activityFooter = document.createElement("div");
 	const activityBody = document.createElement("div");
 	const moveButton = document.createElement("button");
 	const removeButton = document.createElement("button");
 	const activity = document.createElement("p");
+	const controlAttribute = document.createAttribute("data-control");
 
-	activity.innerText = activityName;
+	// Questo attributo data-control identifica la card anche in seguito.
+	controlAttribute.value = activityName.replaceAll(" ", "-");
+	activityCard.setAttributeNode(controlAttribute);
 
 	activityCard.classList.add("card", "todo-card");
 	activityFooter.classList.add("activity-header");
 	activityBody.classList.add("activity-body");
 
 	moveButton.classList.add("move-activity-button");
-	moveButton.innerText = "< - >";
 	removeButton.classList.add("remove-activity-button");
-	removeButton.innerText = "X";
+	moveButton.innerText = "↔️ Sposta";
+	removeButton.innerText = "🗑️ Cancella";
+
+	// Al click, la card viene spostata tra le due liste.
+	moveButton.addEventListener("click", () => {
+		moveActivity(activityCard);
+	});
+
+	// Al click, la card viene rimossa dalla pagina.
+	removeButton.addEventListener("click", () => {
+		removeActivity(activityCard);
+	});
 
 	activityFooter.appendChild(moveButton);
 	activityFooter.appendChild(removeButton);
 
+	activity.innerText = activityName;
 	activityBody.appendChild(activity);
 
 	activityCard.appendChild(activityBody);
 	activityCard.appendChild(activityFooter);
 	todoList.appendChild(activityCard);
 
-	updateActivityList();
+	cardCounter.innerText = String(Number(cardCounter.innerText) + 1);
 }
 
 function moveActivity(card) {
-	completedList.appendChild(card);
+	const todoList = document.getElementById("todo-list");
+	const completedList = document.getElementById("completed-list");
 
-	card.classList.toggle("todo-card");
+	// Se la card è già completata, torna nella lista "todo".
+	if (card.classList.contains("completed-card")) {
+		// appendChild(), se l'elemento esiste già, lo rimuove dal vecchio nodo.
+		todoList.appendChild(card);
+	} else {
+		completedList.appendChild(card);
+	}
+
+	// La classe completed-card indica visivamente e logicamente lo stato della card.
 	card.classList.toggle("completed-card");
 }
 
-function removeActivity() {}
+function removeActivity(card) {
+	card.remove();
 
-function updateActivityList() {}
+	cardCounter.innerText = String(Number(cardCounter.innerText) - 1);
+}
 
 addButton.addEventListener("click", () => {
 	const input = document.getElementById("activity-input");
 	tryAddActivity(input.value);
+
 	input.value = "";
 });
-moveButton.addEventListener("click", () => {
-	moveActivity();
-});
-removeButton.addEventListener("click", () => {
-	removeActivity();
+
+clearButton.addEventListener("click", () => {
+	const todoList = document.getElementById("todo-list");
+	const completedList = document.getElementById("completed-list");
+
+	// Fa il replace con niente, quindi le svuota
+	todoList.replaceChildren();
+	completedList.replaceChildren();
+
+	cardCounter.innerText = String(0);
 });
