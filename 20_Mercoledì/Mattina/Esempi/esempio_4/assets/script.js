ba443e8369c562723d092f296a9a8541ed192bb3
Riccardo Pastori <ricpastori@icloud.com>
2026-05-20 12:41:35 +0200

    Insert add and remove class button in last morning example

--- 20_Mercoledì/Mattina/Esempi/esempio_4/assets/script.js
+++ 20_Mercoledì/Mattina/Esempi/esempio_4/assets/script.js
@@ -1,7 +1,19 @@
-const bottone = document.getElementById("btn");
+const toggleBtn = document.getElementById("btn-toggle");
+const addBtn = document.getElementById("btn-add");
+const removeBtn = document.getElementById("btn-remove");
 const testo = document.getElementById("text");
 
-bottone.addEventListener("click", () => {
+toggleBtn.addEventListener("click", () => {
 	// Aggiunge o rimuove la classe CSS
 	testo.classList.toggle("highlight");
+});
+
+addBtn.addEventListener("click", () => {
+	// Aggiunge la classe CSS
+	testo.classList.add("highlight");
+});
+
+removeBtn.addEventListener("click", () => {
+	// Rimuove la classe CSS
+	testo.classList.remove("highlight");
 });
